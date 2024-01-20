4a1e7081a0bb70188d2e63dc036b80a6acc8c3e0
Maaz Qureshi <maazx.qureshi@mail.utoronto.ca>
2024-01-20 14:51:39 -0500

    Finished laser prototype with bullet counts

--- Prototyping/button_npn_limit/button_npn_limit.ino
+++ Prototyping/button_npn_limit/button_npn_limit.ino
@@ -1,24 +1,32 @@
-#include <time.h>
+#define COOLDOWN 5000
+#define NPN_PIN 2
+#define BUTTON 4
 
-#define COOLDOWN 3
-#define ACTIVE 1
+#define BULLETS 1000
+
+unsigned int num_bullets;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2,OUTPUT);
-  pinMode(4, INPUT);
+  num_bullets = BULLETS;
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Turning on");
-  digitalWrite(2, HIGH);
-  // if(analogRead(4) == HIGH) {
 
-  // }
-  delay(5000);
-  Serial.println("Turning OFF");
-  digitalWrite(2, LOW);
-  delay(5000);
+  if(num_bullets == 0) {
+    digitalWrite(2, LOW);
+    delay(COOLDOWN);
+    num_bullets = BULLETS;
+  }
+
+  int8_t btn = digitalRead(BUTTON);
+  
+  if(btn == HIGH) {
+    num_bullets -= 1;
+    Serial.println(num_bullets);
+    digitalWrite(2, HIGH);
+  }
+  else {
+    digitalWrite(2, LOW);
+  }
 }
