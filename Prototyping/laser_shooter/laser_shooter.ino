50e48c904636c98fe771913f81a561b06cee676a
Maaz Qureshi <maazx.qureshi@mail.utoronto.ca>
2024-02-03 14:08:36 -0500

    Laser shooter with OLED working. Added hearts display code

--- Prototyping/laser_shooter/laser_shooter.ino
+++ Prototyping/laser_shooter/laser_shooter.ino
@@ -2,18 +2,45 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define COOLDOWN 5000
-#define NPN_PIN 2
-#define BUTTON_PIN 4
+#define COOLDOWN 7000
+#define NPN_PIN 7
+#define BUTTON_PIN 5
+#define RELOAD_PIN 3
 #define BULLETS 200
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define NUM_BULLETS 200
 
+int8_t btn;
 unsigned int bulletCount;
 // declare an SSD1306 display object connected to I2C
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 //oled is the name of the OLED object we just constructed
+
+void displayBullets(int bullets) {
+  oled.clearDisplay(); //remember to clear screen
+  oled.setTextSize(4);          // text size
+  oled.setTextColor(WHITE);     // text color
+  oled.setCursor(30, 10);        // position to display
+  oled.println(bullets); // text to display
+  oled.display();
+}
+
+void displayText(const char* text) {
+  oled.clearDisplay(); //clear display
+  oled.setTextSize(2);          // text size
+  oled.setTextColor(WHITE);     // text color
+  oled.setCursor(0, 0);        // position to display
+  oled.println(text);
+  oled.display();
+}
+
+void reload() {
+    Serial.println("Reloading");
+    displayText("Reloading");
+    delay(1000);
+    displayBullets(200);
+    bulletCount = BULLETS;
+}
 
 void setup() {
   bulletCount = BULLETS;
@@ -23,38 +50,37 @@ void setup() {
   if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     while (true);
-  } 
+  }
+  digitalWrite(8, HIGH); // comment out when real laser is used
+  pinMode(RELOAD_PIN, INPUT);
+  displayBullets(200);
+
 }
 
 void loop() {
-
-  oled.clearDisplay(); //clear display
-  oled.setTextSize(4);          // text size
-  oled.setTextColor(WHITE);     // text color
-  oled.setCursor(30, 10);        // position to display
+  
+  if(digitalRead(RELOAD_PIN)) {
+    reload();
+  }
 
   if(bulletCount == 0) {
-    digitalWrite(2, LOW);
-    Serial.print("Reloading");
-    oled.setCursor(30, 10);        // position to display
-    oled.println(bulletCount); // text to display
+    digitalWrite(NPN_PIN, LOW);
+    Serial.println("bullet count 0");
+    displayText("Reloading");
     delay(COOLDOWN);
     bulletCount = BULLETS;
-    oled.clearDisplay(); //remember to clear screen
+    displayBullets(bulletCount);
   }
 
-  int8_t btn = digitalRead(BUTTON_PIN);
+  btn = digitalRead(BUTTON_PIN);
   
-  if(btn == HIGH) {
+  if(btn == HIGH && bulletCount > 0) {
     bulletCount -= 1;
-    oled.setCursor(30, 10);        // position to display
-    oled.println(bulletCount); // text to display
-    oled.display();
+    displayBullets(bulletCount);
     Serial.println(bulletCount);
-    digitalWrite(2, HIGH);
-    oled.clearDisplay(); //remember to clear screen
+    digitalWrite(NPN_PIN, HIGH);
   }
   else {
-    digitalWrite(2, LOW);
+    digitalWrite(NPN_PIN, LOW);
   }
 }
