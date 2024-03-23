6b6aea403223ceb2a20b758c4c69ff665262872a
Maaz Qureshi <maazx.qureshi@mail.utoronto.ca>
2024-03-23 14:47:11 -0400

    WIP test zombie code

--- Prototyping/Boss Zombie/test/test/test.ino
+++ Prototyping/Boss Zombie/test/test/test.ino
@@ -11,69 +11,79 @@
 #include <Servo.h>
 
 // pins for limit switches !!
-#define DETECT1 5 // limit switch on right side
-#define DETECT2 6 // limit switch on left side
+#define DETECT1 6  // limit switch on right side
+#define DETECT2 7  // limit switch on left side
+// #define DETECT3 8 // limit switch on right side
+// #define DETECT4 9 // limit switch on left side
 
 // BOSS ZOMBIE AND ALL PARTS - 2 LDRS, 1 LED, 1 Servo
-const int STATUS_LED_PIN = 11;     // Status LED pin
-const int SENSOR_PIN = A1;        // Photoresistor pin
-const int SENSOR_PIN2 = A0;        // Photoresistor pin the other one
+const int STATUS_LED_PIN = 11;  // Status LED pin
+const int SENSOR_PIN = A1;      // Photoresistor pin
+const int SENSOR_PIN2 = A0;     // Photoresistor pin the other one
 const int SERVO_PIN = 7;
-Servo servo1_boss_zombie; // create servo object
+Servo servo1_boss_zombie;  // create servo object
 
 // STEPPER - moves side to side
-const int stepPin = 3;
-const int dirPin = 2;
-const int stepsPerRevolution = 2000; // we tried 1000 and it was half of the belt
-int curr_pos = stepsPerRevolution/2; // assume starting position is right in the middle
+const int stepPin = 2;
+const int dirPin = 3;
+// Define stuff for only STEPPER2
+const int stepPin2 = 4;
+const int dirPin2 = 5;
+const int stepsPerRevolution = 1000;    // we tried 1000 and it was half of the belt
+int curr_pos = stepsPerRevolution / 2;  // assume starting position is right in the middle
 
 // Setting up objects and values
-int lightThres = 200; // Photoresistor reading values
-int ZOMBIE_STATE = 1; // 1-moving right/clockwise, 0-moving left/CCW, 2-not walking
-int pos_servo1 = 0; // Zombie is turned to the right (walking right)
-int health = 3; //health status
+int lightThres = 200;  // Photoresistor reading values
+int ZOMBIE_STATE = 1;  // 1-moving right/clockwise, 0-moving left/CCW, 2-not walking
+int pos_servo1 = 0;    // Zombie is turned to the right (walking right)
+int health = 3;        //health status
 
 bool GAME_OVER = false;
 
-void setup()
-{
-	// Declare pins as Outputs (stepper)
-	pinMode(stepPin, OUTPUT);
-	pinMode(dirPin, OUTPUT);
+void setup() {
+  // Declare pins as Outputs (stepper)
+  pinMode(stepPin, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
 
   // Initialise BOSS ZOMBIE
   pinMode(STATUS_LED_PIN, OUTPUT);
   servo1_boss_zombie.attach(SERVO_PIN);
 
   // set the zombie into walking to the right mode
-  digitalWrite(STATUS_LED_PIN, LOW); // will be high when it's eating
-	digitalWrite(dirPin, HIGH); // ASSUME THIS IS THE RIGHT WAY OTHERWISE WILL SWITCH
+  digitalWrite(STATUS_LED_PIN, LOW);  // will be high when it's eating
+  digitalWrite(dirPin, HIGH);         // ASSUME THIS IS THE RIGHT WAY OTHERWISE WILL SWITCH
+  digitalWrite(dirPin2, HIGH);        // ASSUME THIS IS THE RIGHT WAY OTHERWISE WILL SWITCH
+
 
   // define input pins for limit switches !!
-  pinMode(DETECT1, INPUT);
-  pinMode(DETECT2, INPUT);
+  pinMode(DETECT1, INPUT_PULLUP);
+  pinMode(DETECT2, INPUT_PULLUP);
 
   Serial.begin(9600);
 }
 
 
-void loop()
-{
+void loop() {
   // to start the zombie will begin to walk in one direction, it takes one step
-  if(ZOMBIE_STATE == 1 ){ // moving right
+  if (ZOMBIE_STATE == 1) {  // moving right
     digitalWrite(STATUS_LED_PIN, LOW);
     // to start the zombie will begin to walk in one direction, it takes one step
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
+    digitalWrite(stepPin2, HIGH);
+    delayMicroseconds(2500);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-    curr_pos = curr_pos +1;
+    digitalWrite(stepPin2, LOW);
+    delayMicroseconds(2500);
+    curr_pos = curr_pos + 1;
 
     // check photo res1 too see if got hit on this side
     int sensorValue = analogRead(SENSOR_PIN);
     bool gotHit = (sensorValue > lightThres);
-    if (gotHit) { // zombie now changes direction
+    if (gotHit) {  // zombie now changes direction
       digitalWrite(dirPin, LOW);
+      digitalWrite(dirPin2, LOW);
       ZOMBIE_STATE = 0;
       servo1_boss_zombie.write(180);
     }
@@ -86,34 +96,40 @@ void loop()
     //  }
 
     //check if zombie has reached the end !!
-  int detected1 = digitalRead(DETECT1); // read right limit switch !!
- 
-  //Serial.println(detected1);
-  //Serial.println(detected2);
-  if (detected1 == 1) // if zombie reaches right side, turn to go to the left !!
-  {
-    health -= 1;
-    digitalWrite(dirPin, LOW);
-    ZOMBIE_STATE = 0;
-    servo1_boss_zombie.write(180);
-    // add code for sunflower to go down, and then come up again
-  }
+    int detected1 = digitalRead(DETECT1);  // read right limit switch !!
 
+    //Serial.println(detected1);
+    //Serial.println(detected2);
+    if (detected1 == 1)  // if zombie reaches right side, turn to go to the left !!
+    {
+      health -= 1;
+      digitalWrite(dirPin, LOW);
+      digitalWrite(dirPin2, LOW);
+      ZOMBIE_STATE = 0;
+      servo1_boss_zombie.write(180);
+      // add code for sunflower to go down, and then come up again
+    }
   }
-  if(ZOMBIE_STATE == 0 ){ // moving left
+  if (ZOMBIE_STATE == 0) {  // moving left
     digitalWrite(STATUS_LED_PIN, HIGH);
     // to start the zombie will begin to walk in one direction, it takes one step
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000); // longer delay -> walk slower
+    digitalWrite(stepPin2, HIGH);
+
+    delayMicroseconds(2500);  // longer delay -> walk slower
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-    curr_pos = curr_pos -1;
+    digitalWrite(stepPin2, LOW);
+
+    delayMicroseconds(2500);
+    curr_pos = curr_pos - 1;
 
     // check photo res2 too see if got hit on this side
     int sensorValue = analogRead(SENSOR_PIN2);
     bool gotHit = (sensorValue > lightThres);
-    if (gotHit) { // zombie now changes direction
+    if (gotHit) {  // zombie now changes direction
       digitalWrite(dirPin, HIGH);
+      digitalWrite(dirPin2, HIGH);
+
       ZOMBIE_STATE = 1;
       servo1_boss_zombie.write(0);
     }
@@ -125,21 +141,19 @@ void loop()
     //    servo1_boss_zombie.write(0);
     //  }
 
-  int detected2 = digitalRead(DETECT2); // read left limit switch !!
-  if(detected2 == 1) // if zombie reaches left side, turn to go to the right !!
-  {
-    health -= 1;
-    digitalWrite(dirPin, HIGH);
-    ZOMBIE_STATE = 1;
-    servo1_boss_zombie.write(0); 
-    // add code for sunflower to go down, and then come up again
+    int detected2 = digitalRead(DETECT2);  // read left limit switch !!
+    if (detected2 == 1)                    // if zombie reaches left side, turn to go to the right !!
+    {
+      health -= 1;
+      digitalWrite(dirPin, HIGH);
+      digitalWrite(dirPin2, HIGH);
+      ZOMBIE_STATE = 1;
+      servo1_boss_zombie.write(0);
+      // add code for sunflower to go down, and then come up again
+    }
   }
 
-  }
-
-  if(health == 0)
-  {
+  if (health == 0) {
     GAME_OVER = true;
   }
-
 }
